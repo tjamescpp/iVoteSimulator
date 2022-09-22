6991d39f8e497afed52ed4d73ab0ee5866a15d7b
tjamescpp <ttjames@cpp.edu>
2022-09-22 00:08:05 -0700

    Initial Project Version

---
+++ SimulationDriver.java
@@ -0,0 +1,15 @@
+package iVoteSimulator;
+
+/*
+ * Simulation driver class
+ * 
+ * 1) create a question type and configure the answers; 
+ * 2) configure the question for iVote Service;
+ * 3) randomly generate a number students and the answers;
+ * 4) submit all the students’ answers to iVote Service; 
+ * 5) call the Voting Service output function to display the result.
+ */
+
+public class SimulationDriver {
+
+}
