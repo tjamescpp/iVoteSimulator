d06c798270ff8b107e3808ee086cd159caf347a9
tjamescpp <ttjames@cpp.edu>
2022-09-26 14:27:09 -0700

    Student class implemented

--- Student.java
+++ Student.java
@@ -1,9 +1,43 @@
 package iVoteSimulator;
 
+import java.util.Random;
+
 /*
  * Student class
  */
 
 public class Student {
+    private String name;
+    private int studentID;
+    private Random randInt;
+
+    public Student(String name) {
+        this.name = name;
+        // init random studentID
+        this.studentID = randomID();
+    }
+
+    // generates random number between (1000, 9999)
+    private int randomID(){
+        randInt = new Random();
+        return randInt.nextInt(10000) + 1000;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
     
 }
